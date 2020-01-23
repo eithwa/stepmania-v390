df5d88724243a616112103694d0a6fb74add023c
eithwa <mich841012@gmail.com>
2020-01-23 18:19:39 +0800

    init file

---
+++ src/disasm/verstub.cpp
@@ -0,0 +1,2 @@
+unsigned long version_num = 1;
+const char *version_time = "Wed Jan 22 17:00:39 2020";
---
+++ src/verstub.cpp
@@ -0,0 +1,2 @@
+unsigned long version_num = 166;
+const char *version_time = "Thu Jan 23 17:28:46 2020";
